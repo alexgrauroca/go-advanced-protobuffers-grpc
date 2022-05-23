40c5389fd7e76c8b7e35e64c1211c00d811089f3
Àlex Grau Roca <alexgrauroca91@gmail.com>
2022-05-23 08:25:25 +0200

    Client streaming

--- client/main.go
+++ client/main.go
@@ -20,6 +20,7 @@ func main() {
 
 	c := testpb.NewTestServiceClient(cc)
 	DoUnary(c)
+	DoClientStreaming(c)
 }
 
 func DoUnary(c testpb.TestServiceClient) {
@@ -33,5 +34,47 @@ func DoUnary(c testpb.TestServiceClient) {
 		log.Fatalf("Error while calling GetTest: %v", err)
 	}
 
-	log.Printf("GetTest response: %v\n", res)
+	log.Printf("GetTest response: %v", res)
+}
+
+func DoClientStreaming(c testpb.TestServiceClient) {
+	questions := []*testpb.Question{
+		{
+			Id:       "q8t1",
+			Answer:   "lijasd",
+			Question: "lijasdsad",
+			TestId:   "t1",
+		},
+		{
+			Id:       "q9t1",
+			Answer:   "owiqeu",
+			Question: "oiwque",
+			TestId:   "t1",
+		},
+		{
+			Id:       "q10t1",
+			Answer:   "mnsd",
+			Question: "okkwqwwwwww",
+			TestId:   "t1",
+		},
+	}
+
+	stream, err := c.SetQuestions(context.Background())
+
+	if err != nil {
+		log.Fatalf("Error while calling SetQuestions: %v", err)
+	}
+
+	for _, question := range questions {
+		log.Println("Sending question: ", question.GetId())
+		stream.Send(question)
+	}
+
+	msg, err := stream.CloseAndRecv()
+
+	if err != nil {
+		log.Fatalf("Error while closing stream: %v", err)
+	}
+
+	log.Printf("response from server: %v", msg)
 }
