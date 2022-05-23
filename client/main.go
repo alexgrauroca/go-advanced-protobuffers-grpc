90a72f00d9960ac8044ab04bb606af6997dee477
Àlex Grau Roca <alexgrauroca91@gmail.com>
2022-05-23 08:07:08 +0200

    Starting client side

---
+++ client/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	cc, err := grpc.Dial("localhost:5070", grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+
+	defer cc.Close()
+}
