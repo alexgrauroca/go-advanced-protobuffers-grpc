68a0310330c186cc53f35664522c52e663880690
Àlex Grau Roca <alexgrauroca91@gmail.com>
2022-05-23 08:19:48 +0200

    Unary request

--- client/main.go
+++ client/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"platzi.com/go/grpc/testpb"
 )
 
 func main() {
@@ -15,4 +17,21 @@ func main() {
 	}
 
 	defer cc.Close()
+
+	c := testpb.NewTestServiceClient(cc)
+	DoUnary(c)
+}
+
+func DoUnary(c testpb.TestServiceClient) {
+	req := &testpb.GetTestRequest{
+		Id: "t1",
+	}
+
+	res, err := c.GetTest(context.Background(), req)
+
+	if err != nil {
+		log.Fatalf("Error while calling GetTest: %v", err)
+	}
+
+	log.Printf("GetTest response: %v\n", res)
 }
